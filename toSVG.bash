94ff7826f7b2f6638c032245d2bb6dfc35859bba
Danyel Fisher <danyelf@microsoft.com>
2017-10-13 16:49:02 -0700

    Added config: background to all images.  May want to search and replace into "white"

--- toSVG.bash
+++ toSVG.bash
@@ -1,17 +1,24 @@
 #!/bin/bash
 rm -rf svg
 mkdir svg
-for vg in spec/*.vg.json
+
+echo "---- PROCESSING VEGA FILES ---"
+for vg in $(ls spec/*.vg.json | sort -n)
 do
-	echo $vg
     jsonName=svg/$(basename -s .vg.json $vg).svg
-    vg2svg $vg > $jsonName
+    vg2svg $vg > $jsonName 2> errorlog.txt || echo $vg "failed VG"
 done
-for vl in spec/*.vl.json
+echo "---- PROCESSING VEGA-LITE FILES ---"
+for vl in $(ls spec/*.vl.json | sort -n)
 do
-	echo $vl
     jsonName=svg/$(basename -s .vl.json $vl).svg
-    vl2svg $vl > $jsonName
+    vl2svg $vl > $jsonName 2> errorlog.txt || echo $vl "failed VL"
 done
+
+echo "---- FAILED FILES ---"
+find svg -size 0 
+find svg -size 0 -print0 | xargs -0 rm
+echo "---- Copying to book release dir ... ---"
 cp svg/singleview*.svg ../making_sense_of_data/images/singleview
 cp svg/multiview*.svg ../making_sense_of_data/images/multiview
+
